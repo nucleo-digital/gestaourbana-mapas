800eb99b1d1a6fe26aed5ea50d23bcf9db7c566b
Lucas Pirola <lucaspirola@gmail.com>
2014-10-11 02:17:08 -0300

    pegando variavel de ambiente para porta do express por causa do heroku

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ var config = module.exports;
 var PRODUCTION = process.env.NODE_ENV === "production";
 
 config.express = {
-    port: process.env.EXPRESS_PORT || 3000,
+    port: process.env.PORT || 3000,
     ip: "127.0.0.1"
 };
 
